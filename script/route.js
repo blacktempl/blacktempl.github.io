1f20ccb8e7116301fcc516acb3a24ffb44b0ddac
Igor Strelnyk <rodriguezz4060@gmail.com>
2021-08-05 09:05:18 +0300

    route

--- script/route.js
+++ script/route.js
@@ -2,7 +2,7 @@ const affixes = [
   { name: "Fortified-Inspiring-Necrotic", href: "/guide/route/Fortified-Inspiring-Necrotic.html" },
   { name: "Tyrannical-Spiteful-Necrotic", href: "/guide/route/Tyrannical-Spiteful-Necrotic.html" },
   { name: "Tyrannical-Bursting-Explosive", href: "/guide/route/Tyrannical-Bursting-Explosive.html" },
-  { name: "Fortified-Sanguine-Grievous", href: "/guide/route/Fortified-Sanguine-Grievous.html" },
+  { name: "Fortified-Sanguine-Grievous", href: "/guide/route/Tyrannical-Storming-Sanguine.html" },
   { name: "Tyrannical-Raging-Quaking", href: "/guide/route/Tyrannical-Raging-Quaking.html" },
   { name: "Fortified-Bursting-Volcanic", href: "/guide/route/Fortified-Bursting-Volcanic.html" },
   { name: "Tyrannical-Bolstering-Storming", href: "/guide/route/Tyrannical-Bolstering-Storming.html" },
